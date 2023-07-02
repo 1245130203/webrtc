93a2cd946b2705fab47b54954df4c64c12131971
shensiming <shensiming@bunanapp.com>
2023-07-02 16:11:37 +0800

    feat: 完成一个最简单的web服务

--- service/index.js
+++ service/index.js
@@ -1,7 +1,14 @@
 'use strict'
 var http = require('http');
+var fs = require('fs');
+var express = require('express');
+var serveIndex = require('serve-index');
 
-var app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('hello world\n');
-}).listen(8091, '0.0.0.0');
+var app = express();
+app.use(serveIndex('./public'));
+app.use(express.static('./public'));
+
+
+// var http_server = http.createServer(app).listen(8091, '0.0.0.0');
+
+var http_server = http.createServer(app).listen(8091, '0.0.0.0');
